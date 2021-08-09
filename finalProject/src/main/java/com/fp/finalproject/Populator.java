9f528cc167876735e6e161d3902c18cf6b1b1503
Coach-David <6dpatt@gmail.com>
2021-08-08 22:18:03 -0400

    more populator

--- finalProject/src/main/java/com/fp/finalproject/Populator.java
+++ finalProject/src/main/java/com/fp/finalproject/Populator.java
@@ -33,6 +33,12 @@ public class Populator implements CommandLineRunner {
         christian.addPhoto("https://scontent.fcmh1-1.fna.fbcdn.net/v/t39.30808-6/214859654_5847284395313018_6272224978439311304_n.jpg?_nc_cat=101&ccb=1-4&_nc_sid=730e14&_nc_ohc=fVPxdJ59IAMAX9c_5T9&_nc_ht=scontent.fcmh1-1.fna&oh=825d1f8c18291caaf4aea59e9ae05dd2&oe=6115954D");
         proStorage.savePro(christian);
 
+        Serve razorShave = new Serve("Razor-Shave", "Clean razor shave.", 9.99);
+        serveStorage.saveService(razorShave);
+        christian.addReview("Baby smooth.");
+        christian.addPhoto("https://st2.depositphotos.com/3261171/7015/i/950/depositphotos_70152053-stock-photo-client-shaving-at-barber-shop.jpg");
+        proStorage.savePro(christian);
+
         Serve twoToneTattoo = new Serve("Black and White Tattoo", "Black and white tattoo", 75.00);
         serveStorage.saveService(twoToneTattoo);
         Professional heather = new Professional("Heather", "Really great tattoos", 5, "Filler", true, "1234836262", "cash", "Weekdays");
@@ -43,15 +49,41 @@ public class Populator implements CommandLineRunner {
         Serve colorTattoo = new Serve("Colored Tattoo", "Colored Tattoo", 80.00);
         serveStorage.saveService(colorTattoo);
         heather.addReview("My tattoo is looking great!");
-        heather.addPhoto("");
+        heather.addPhoto("https://www.tattoosfolder.com/wp-content/uploads/2020/07/1-elephant-and-lotus-tattoo-on-dark-skin.jpg?x50638");
         proStorage.savePro(heather);
 
         Serve hairStyle = new Serve("Hair-Style", "Hair-styling", 20.00);
         serveStorage.saveService(hairStyle);
         Professional bri = new Professional("Bri", "Good stylist", 5, "Filler", true, "1234566643", "cash", "Weekdays");
         bri.addReview("Great style!");
-        bri.addPhoto("");
+        bri.addPhoto("https://www.toptrendsguide.com/wp-content/uploads/2020/03/Long-Hairstyles-For-Women.jpg");
         proStorage.savePro(bri);
+
+        Serve hairColor = new Serve("Color Hair", "We offer a variety of color options.", 50.00);
+        serveStorage.saveService(hairColor);
+        bri.addReview("My no more roots!");
+        bri.addPhoto("https://phiasalon.com/wp-content/themes/phia-salon-custom-theme/assets/img/home-page--color-v2.jpg");
+        proStorage.savePro(bri);
+
+        Serve hairHighlight = new Serve("Hair-Highlight", "Trendy and stylish, highlights are a great option for spicing up you look!.", 50.00);
+        serveStorage.saveService(hairHighlight);
+        bri.addReview("My hair is really light!");
+        bri.addPhoto("https://phiasalon.com/wp-content/themes/phia-salon-custom-theme/assets/img/services-page--highlights-v2.jpg");
+        proStorage.savePro(bri);
+
+        Serve gelNails = new Serve("Gel nails", "I have gel nails now!", 30.00);
+        serveStorage.saveService(gelNails);
+        Professional mandy = new Professional("Mandy", "Good nails", 5, "Filler", true, "32487641314", "cash", "Weekdays");
+        mandy.addReview("She did my nails!");
+        mandy.addPhoto("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/download-3-1585685825.jpeg?crop=1.00xw:0.502xh;0,0.329xh&resize=1200:*");
+        proStorage.savePro(mandy);
+
+        Serve acrylicNails = new Serve("Acrylic Nails", "My nails are acrylic!", 30.00);
+        serveStorage.saveService(acrylicNails);
+        mandy.addReview("My nails look amazing!");
+        mandy.addPhoto("https://www.womansworld.com/wp-content/uploads/2018/12/acrylic-nails-1.jpg");
+        proStorage.savePro(mandy);
+
 
     }
 
