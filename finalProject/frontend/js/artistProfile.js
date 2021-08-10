3dc9072a11fb0edf2bb5e36cafe81098d45e5750
HEWiggins <HE.Wiggins1990@gmail.com>
2021-08-10 15:51:51 -0400

    started artistProfile.js. Needs to apppend.

---
+++ finalProject/frontend/js/artistProfile.js
@@ -0,0 +1,119 @@
+const artistName = document.createElement("h1");
+artistName.classList("artist-name");
+
+const contentCards = document.createElement("div");
+contentCards.classList("content-cards");
+
+const contentCardImg = document.createElement(:)
+
+
+
+
+
+
+
+{/* <h1 class="artist-name" >Angelo Moore</h1>
+
+        <div class="content-cards">
+            <div class="content-cards_img">
+                <img src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+                    alt="">
+            </div>
+            <div class="content-cards_info">
+                <div class="content-cards_title">
+                    <span class="occupation">Occupation: Hair Stylist</span>
+                    <br>
+                    <span class="years">Years of Service: 7</span>
+                    <br>
+                    <span class="overall-rating">Overall Rating: 5 Stars</span>
+                    <br>
+                    <span class="location">Location: Good Life Barber Shop</span>
+                    <br>
+                    <span class="about-artist">About the Artist:</span>
+                    <p class="artist-info">Every day you walk into work, you will socialize. Barbershops are known as a place for folx to go
+                        hangout and socialize while getting a haircut. You have the opportunity to create a fun
+                        environment with your clients and create a social network with them.</p>
+                </div> */}
+
+
+
+
+
+
+// --------------------------------------------------
+
+const modalContainer = document.createElement("div");
+modalContainer.classList.add("modal-container");
+const scheduleModal = document.createElement("div");
+scheduleModal.classList.add("modals");
+const scheduleBtn = document.createElement("button");
+scheduleBtn.innerText = "Scheduling / Hours of Opperations";
+scheduleBtn.addEventListener("click", () =>{
+    const scheduleTab = window.open('http://google.com', '_self');
+})
+
+const reviewModal = document.createElement("div");
+reviewModal.classList.add("modals");
+const reviewBtn = document.createElement("button");
+reviewBtn.innerText = "Review & Rating";
+reviewBtn.addEventListener("click", () =>{
+    const reviewTab = window.open('http://google.com', '_self');
+})
+
+const messageModal = document.createElement("div");
+messageModal.classList.add("modals");
+const messageBtn = document.createElement("button");
+messageBtn.innerText = "Message Me";
+messageBtn.addEventListener("click", () =>{
+    const messageTab = window.open('http://google.com', '_self');
+})
+
+const lastModal = document.createElement("div");
+lastModal.classList.add("modals");
+const lastBtn = document.createElement("button");
+lastBtn.innerText = "Last Modal";
+lastBtn.addEventListener("click", () =>{
+    const lastTab = window.open('http://google.com', '_self');
+})
+
+
+
+
+
+
+{/* 
+                    
+                        <button onclick="document.getElementById('id01').style.display='block'"
+                            class="content-cards_cta">Scheduling/Hours of Opp</button>
+                    </div>
+                    <div class="modals">
+                        <button onclick="document.getElementById('id01').style.display='block'"
+                            class="content-cards_cta">Reviews & Ratings</button>
+                    </div>
+                    <div class="modals">
+                        <button onclick="document.getElementById('id01').style.display='block'"
+                            class="content-cards_cta">Message Me</button>
+                    </div>
+                    <div class="modals">
+                        <button onclick="document.getElementById('id01').style.display='block'"
+                            class="content-cards_cta">Scheduling</button>
+                    </div>
+                </div> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
