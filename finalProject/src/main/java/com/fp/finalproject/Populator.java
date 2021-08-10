ab036d41bf954531d290702f336eedd29fca5ae6
TheSamYoder <syoder93@gmail.com>
2021-08-10 11:18:35 -0400

    updated populator and pojos

--- finalProject/src/main/java/com/fp/finalproject/Populator.java
+++ finalProject/src/main/java/com/fp/finalproject/Populator.java
@@ -19,72 +19,76 @@ public class Populator implements CommandLineRunner {
     }
 
     @Override public void run(String...args) throws Exception{
-        Serve cutHair = new Serve("Hair-cut", "Standard Hair-Cut", "$");
-        serveStorage.saveService(cutHair);
-        Serve beardTrim = new Serve("Beard-Trim","Standard Beard-trim","$");
-        serveStorage.saveService(beardTrim);
-        Serve razorShave = new Serve("Razor-Shave", "Clean razor shave.", "$");
-        serveStorage.saveService(razorShave);
-        Serve twoToneTattoo = new Serve("Black and White Tattoo", "Black and white tattoo", "$$$");
-        serveStorage.saveService(twoToneTattoo);
-        Serve colorTattoo = new Serve("Colored Tattoo", "Colored Tattoo", "$$$");
-        serveStorage.saveService(colorTattoo);
-        Serve acrylicNails = new Serve("Acrylic Nails", "My nails are acrylic!", "$$");
-        serveStorage.saveService(acrylicNails);
-        Serve gelNails = new Serve("Gel nails", "I have gel nails now!", "$$");
-        serveStorage.saveService(gelNails);
-        Serve hairHighlight = new Serve("Hair-Highlight", "Trendy and stylish, highlights are a great option for spicing up you look!.", "$$");
-        serveStorage.saveService(hairHighlight);
-        Serve hairColor = new Serve("Color Hair", "We offer a variety of color options.", "$$");
-        serveStorage.saveService(hairColor);
-        Serve hairStyle = new Serve("Hair-Style", "Hair-styling", "$$");
-        serveStorage.saveService(hairStyle);
-
-
 //      Christian
-        Professional christian = new Professional("Christian","Cool dude!", 5, "Filler", true, "123456789", "cash", "Weekdays", cutHair, beardTrim, razorShave);
+        Professional christian = new Professional("Christian","Cool dude!", 5.3, "Filler", true, "Barber","123456789", "cash", "Weekdays");
         christian.addReview("He's Great at hair cut!");
-        christian.addPhoto("https://scontent.fcmh1-1.fna.fbcdn.net/v/t1.6435-9/212534922_5825309820843809_1839992674517398186_n.jpg?_nc_cat=103&ccb=1-4&_nc_sid=730e14&_nc_ohc=5TqC-cGmO0QAX-u0gHz&_nc_oc=AQlIpuBU94XKc6xJKMtTF579tXmObGuISM82ZMVwvE4RM_4itCZNdOpfVRsATCeknIo&tn=WIGr1hVuDmCaGKYv&_nc_ht=scontent.fcmh1-1.fna&oh=50770e8d60fe8f7a28ef2597b5635575&oe=6133C7B1");
+        christian.addPhoto("https://bit.ly/3siGu4N");
         christian.addReview("My beard looks great!");
-        christian.addPhoto("https://scontent.fcmh1-1.fna.fbcdn.net/v/t39.30808-6/214859654_5847284395313018_6272224978439311304_n.jpg?_nc_cat=101&ccb=1-4&_nc_sid=730e14&_nc_ohc=fVPxdJ59IAMAX9c_5T9&_nc_ht=scontent.fcmh1-1.fna&oh=825d1f8c18291caaf4aea59e9ae05dd2&oe=6115954D");
+        christian.addPhoto("https://bit.ly/3CzDdCy");
         christian.addReview("Baby smooth.");
-        christian.addPhoto("https://st2.depositphotos.com/3261171/7015/i/950/depositphotos_70152053-stock-photo-client-shaving-at-barber-shop.jpg");
+        christian.addPhoto("https://bit.ly/3Az6lZ5");
         proStorage.savePro(christian);
 
 //      Heather
-        Professional heather = new Professional("Heather", "Really great tattoos", 5, "Filler", true, "1234836262", "cash", "Weekdays", twoToneTattoo, colorTattoo);
+        Professional heather = new Professional("Heather", "Really great tattoos", 5.4, "Filler", true, "Tattoo Artist","1234836262", "cash", "Weekdays");
         heather.addReview("She is a great artist!");
-        heather.addPhoto("https://scontent.fcmh1-1.fna.fbcdn.net/v/t1.6435-9/55498138_10157122952957603_7267199598156840960_n.jpg?_nc_cat=108&ccb=1-4&_nc_sid=730e14&_nc_ohc=aDe2oYYUqqAAX9AtTFE&_nc_ht=scontent.fcmh1-1.fna&oh=ca468e7d028dc55d214924ec925ef3d1&oe=6137E6B0");
+        heather.addPhoto("https://bit.ly/3iBOvhM");
         heather.addReview("My tattoo is looking great!");
-        heather.addPhoto("https://scontent.fcmh1-1.fna.fbcdn.net/v/t1.6435-9/172997863_10159362318502603_6220972233347603671_n.jpg?_nc_cat=103&ccb=1-4&_nc_sid=730e14&_nc_ohc=u3rwL_wxBD4AX9arOUL&tn=WIGr1hVuDmCaGKYv&_nc_ht=scontent.fcmh1-1.fna&oh=ce7d068a38f58b7228adf8726ac198dc&oe=6136D97F");
+        heather.addPhoto("https://bit.ly/3CCls5C");
         proStorage.savePro(heather);
 
 //      Bri
-        Professional bri = new Professional("Bri", "Good stylist", 5, "Filler", true, "1234566643", "cash", "Weekdays", hairStyle, hairHighlight, hairColor);
+        Professional bri = new Professional("Bri", "Good stylist", 5.6, "Filler", true, "Hair Stylist","1234566643", "cash", "Weekdays");
         bri.addReview("Great style!");
-        bri.addPhoto("https://www.toptrendsguide.com/wp-content/uploads/2020/03/Long-Hairstyles-For-Women.jpg");
+        bri.addPhoto("https://bit.ly/3lPFnIl");
         bri.addReview("My no more roots!");
-        bri.addPhoto("https://phiasalon.com/wp-content/themes/phia-salon-custom-theme/assets/img/home-page--color-v2.jpg");
+        bri.addPhoto("https://bit.ly/2VzcTYR");
         bri.addReview("My hair is really light!");
-        bri.addPhoto("https://phiasalon.com/wp-content/themes/phia-salon-custom-theme/assets/img/services-page--highlights-v2.jpg");
+        bri.addPhoto("https://bit.ly/2VAzGUc");
         proStorage.savePro(bri);
 
 //      Mandy
-        Professional mandy = new Professional("Mandy", "Good nails", 5, "Filler", true, "32487641314", "cash", "Weekdays", gelNails, acrylicNails);
+        Professional mandy = new Professional("Mandy", "Good nails", 5.1, "Filler", true, "Manicurist","32487641314", "cash", "Weekdays");
         mandy.addReview("She did my nails!");
-        mandy.addPhoto("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/download-3-1585685825.jpeg?crop=1.00xw:0.502xh;0,0.329xh&resize=1200:*");
+        mandy.addPhoto("https://bit.ly/3iAi3wn");
         mandy.addReview("My nails look amazing!");
-        mandy.addPhoto("https://www.womansworld.com/wp-content/uploads/2018/12/acrylic-nails-1.jpg");
+        mandy.addPhoto("https://bit.ly/3yATwwA");
         proStorage.savePro(mandy);
 
 //      Eli
-        Professional eli = new Professional("Eli", "Great hair cuts!", 5, "Filler", true, "1112223333", "cash", "Weekdays", cutHair, beardTrim, razorShave);
+        Professional eli = new Professional("Eli", "Great hair cuts!", 5.0, "Filler", true, "Barber","1112223333", "cash", "Weekdays");
         eli.addReview("He is a great dude and I love his hair cuts!");
-        eli.addPhoto("https://images.unsplash.com/photo-1593702275687-f8b402bf1fb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80");
+        eli.addPhoto("https://bit.ly/2UatSQu");
         eli.addReview("My beard looks great everytime Eli cuts me up!");
-        eli.addPhoto("https://www.humanhairexim.com/wp-content/uploads/2021/04/male-hair-cuts-001.jpg");
+        eli.addPhoto("https://bit.ly/3AphVpi");
         proStorage.savePro(eli);
 
+        Serve cCutHair = new Serve(christian,"Hair-cut", "Standard Hair-Cut", "$");
+        serveStorage.saveServe(cCutHair);
+        Serve cBeardTrim = new Serve(christian,"Beard-Trim","Standard Beard-trim","$");
+        serveStorage.saveServe(cBeardTrim);
+        Serve cRazorShave = new Serve(christian,"Razor-Shave", "Clean razor shave.", "$");
+        serveStorage.saveServe(cRazorShave);
+        Serve eCutHair = new Serve(eli,"Hair-cut", "Hair-Cut", "$");
+        serveStorage.saveServe(eCutHair);
+        Serve eBeardTrim = new Serve(eli,"Beard-Trim","Beard-trim","$");
+        serveStorage.saveServe(eBeardTrim);
+        Serve eRazorShave = new Serve(eli,"Razor-Shave", "Close shave with a razor", "$$");
+        serveStorage.saveServe(eRazorShave);
+        Serve hTwoToneTattoo = new Serve(heather,"Black and White Tattoo", "Black and white tattoo", "$$$");
+        serveStorage.saveServe(hTwoToneTattoo);
+        Serve hColorTattoo = new Serve(heather,"Colored Tattoo", "Colored Tattoo", "$$$");
+        serveStorage.saveServe(hColorTattoo);
+        Serve mAcrylicNails = new Serve(mandy,"Acrylic Nails", "My nails are acrylic!", "$$");
+        serveStorage.saveServe(mAcrylicNails);
+        Serve mGelNails = new Serve(mandy,"Gel nails", "I have gel nails now!", "$$");
+        serveStorage.saveServe(mGelNails);
+        Serve bHairHighlight = new Serve(bri,"Hair-Highlight", "Trendy and stylish, highlights are a great option for spicing up you look!.", "$$");
+        serveStorage.saveServe(bHairHighlight);
+        Serve bHairColor = new Serve(bri,"Color Hair", "We offer a variety of color options.", "$$");
+        serveStorage.saveServe(bHairColor);
+        Serve bHairStyle = new Serve(bri,"Hair-Style", "Hair-styling", "$$");
+        serveStorage.saveServe(bHairStyle);
 
 
     }
