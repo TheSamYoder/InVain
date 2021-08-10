dae83f3a9c5aa5009fcab803256746bc5e37d342
TheSamYoder <syoder93@gmail.com>
2021-08-10 15:21:57 -0400

    updated styling and homepage classes

--- finalProject/frontend/js/landPage.js
+++ finalProject/frontend/js/landPage.js
@@ -1,84 +1,84 @@
 const displayLandingPage = function (professionals) {
 
     const mainContainer = document.createElement("main");
-    mainContainer.classList("landcontainer");
+    mainContainer.classList.add("land-container");
 
     const secHair = document.createElement("section");
-    secHair.classList("secHair");
+    secHair.classList.add("sec-hair");
 
     const hairInfo = document.createElement("div");
-    hairInfo.classList("hairInfo");
+    hairInfo.classList.add("hair-info");
 
     const hairImg = document.createElement("img");
-    hairImg.classList("hairImg");
+    hairImg.classList.add("hair-img");
     hairImg.src = "https://bit.ly/3yD1PIo";
     hairImg.alt = "A barber cutting hair";
 
     const hairWords = document.createElement("div");
-    hairWords.classList("hairWords");
+    hairWords.classList.add("hair-words");
 
     const salonText = document.createElement("h3");
-    salonText.classList("salonText");
+    salonText.classList.add("salon-text");
     salonText.innerText = "Hair Salon";
 
     const barberText = document.createElement("h3");
-    barberText.classList("barberText");
+    barberText.classList.add("barber-text");
     barberText.innerText = "Barbershop";
 
     const bothText = document.createElement("h3");
-    bothText.classList("bothText");
+    bothText.classList.add("both-text");
     bothText.innerText = "Both";
 
     const secNails = document.createElement("section");
-    secNails.classList("secNails");
+    secNails.classList.add("sec-nails");
 
     const nailsInfo = document.createElement("div");
-    nailsInfo.classList("nailsInfo");
+    nailsInfo.classList.add("nails-info");
 
     const nailsImg = document.createElement("img");
-    nailsImg.classList("nailsImg");
+    nailsImg.classList.add("nails-img");
     nailsImg.src = "https://bit.ly/3iAuXKH";
     nailsImg.alt = "A picture of hands and feet";
 
     const nailWords = document.createElement("div");
-    nailWords.classList("nailWords");
+    nailWords.classList.add("nail-words");
 
     const pedText = document.createElement("h3");
-    pedText.classList("pedText");
+    pedText.classList.add("ped-text");
     pedText.innerText = "Pedicure";
 
     const medText = document.createElement("h3");
-    medText.classList("medText");
+    medText.classList.add("man-text");
     medText.innerText = "Manicure";
 
     const styText = document.createElement("h3");
-    styText.classList("styText");
+    styText.classList.add("sty-text");
     styText.innerText = "Style";
 
     const secTat = document.createElement("section");
-    secTat.classList("secTat");
+    secTat.classList.add("sec-tat");
 
     const tattooInfo = document.createElement("div");
-    tattooInfo.classList("tattooInfo");
+    tattooInfo.classList.add("tattoo-info");
 
     const tattooImg = document.createElement("img");
-    tattooImg.classList("tattooImg");
+    tattooImg.classList.add("tattoo-img");
     tattooImg.src = "https://bit.ly/3iAuYOL";
     tattooImg.alt = "A picture of a tattoo artist";
 
     const tattooWords = document.createElement("div");
-    tattooWords.classList("tattooWords");
+    tattooWords.classList.add("tattoo-words");
 
     const newTat = document.createElement("h3");
-    newTat.classList("newTat");
+    newTat.classList.add("new-tat");
     newTat.innerText = "New Tattoo";
 
     const coverUps = document.createElement("h3");
-    coverUps.classList("coverUps");
+    coverUps.classList.add("cover-ups");
     coverUps.innerText = "Cover Ups";
 
     const enhancements = document.createElement("h3");
-    enhancements.classList("enhancements");
+    enhancements.classList.add("enhancements");
     enhancements.innerText = "Enhancements";
 
     hairWords.appendChild(salonText);
