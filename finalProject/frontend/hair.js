0dfd10c049093b04beb97a97ab03689c5017a5e3
dasminew5 <dasminewright5@gmail>
2021-08-10 11:48:02 -0400

    started to build out js for hair page

--- finalProject/frontend/hair.js
+++ finalProject/frontend/hair.js
@@ -1,2 +1,62 @@
+//Filter Element Div
 const filterElement = document.createElement("div");
 filterElement.classList.add("filter-h2");
+const filterBox = document.createElement("div");
+filterBox.classList.add("filter-box");
+const filterButtonContainer = document.createElement("div");
+filterButtonContainer.classList.add("myBtnContainer");
+
+//Filter Buttons
+const showAllButton = document.createElement("button");
+showAllButton.classList.add("btn active");
+showAllButton.innerText = "Show all";
+
+const onlineSchButton = document.createElement("button");
+onlineSchButton.classList.add("btn");
+onlineSchButton.innerText = "Online Scheduling";
+
+const openOnWeekButton = document.createElement("button");
+openOnWeekButton.classList.add("btn");
+openOnWeekButton.innerText = "Open on weekdays";
+
+const openOnWeekendsButton = document.createElement("button");
+openOnWeekendsButton.classList.add("btn");
+openOnWeekendsButton.innerText = "Open on weekends";
+
+const acceptsCashAppBtn = document.createElement("button");
+acceptsCashAppBtn.classList.add("btn");
+acceptsCashAppBtn.innerText = "Accepts Cashapp";
+
+const acceptsCashbtn = document.createElement("button");
+acceptsCashbtn.classList.add("btn");
+acceptsCashbtn.innerText = "Accepts Cash";
+
+const acceptsDebitOrCreditBtn = document.createElement("button");
+acceptsDebitOrCreditBtn.classList.add("btn");
+acceptsDebitOrCreditBtn.innerText = "Accepts Debit/Credit";
+
+const lowestPriceBtn = document.createElement("button");
+lowestPriceBtn.classList.add("btn");
+lowestPriceBtn.innerText = "Lowest Price";
+
+const highestPriceBtn = document.createElement("button");
+highestPriceBtn.classList.add("btn");
+highestPriceBtn.innerText = "Highest Price";
+
+const extraFilterBtnOne = document.createElement("button");
+extraFilterBtnOne.classList.add("btn");
+extraFilterBtnOne.innerText = "Extra Filters";
+
+const extraFilterBtnTwo = document.createElement("button");
+extraFilterBtnTwo.classList.add("btn");
+extraFilterBtnTwo.innerText = "Extra";
+
+//Content Cards 
+const contentCardElement = document.createElement("div");
+contentCardElement.classList.add("content-cards");
+const contentCardImg = document.createElement("div");
+contentCardImg.classList.add("content-cards_img");
+const 
+
+
+
