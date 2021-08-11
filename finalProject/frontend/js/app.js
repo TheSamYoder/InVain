a9dbc17d0e77139672321844c1e73699ea8c5498
dasminew5 <dasminewright5@gmail>
2021-08-11 19:21:52 -0400

    added additional js and a footer.js

--- finalProject/frontend/js/app.js
+++ finalProject/frontend/js/app.js
@@ -3,8 +3,16 @@ import {
 } from "./navbar.js"
 
 import {
+    createFooter
+} from "./footer.js"
+
+import {
     displayLandingPage
 } from "./landPage.js"
+
+import {
+    contentPage
+}   from "./hair.js"
 
 const container = document.querySelector(".container");
 
@@ -12,5 +20,6 @@ const mainContainer = document.createElement("main");
 
 mainContainer.classList.add("main-content");
 container.append(createNavbar());
-mainContainer.appendChild(displayLandingPage());
+// mainContainer.appendChild(displayLandingPage());
+mainContainer.appendChild(contentPage());
 container.appendChild(mainContainer);
--- finalProject/frontend/js/hair.js
+++ finalProject/frontend/js/hair.js
@@ -1,11 +1,15 @@
+const contentPage = function() {
+
 const hairDiv = document.createElement("div"); 
+const bodyTag = document.createElement("body");
 
 const hairTitle = document.createElement("h1");
-hairTitle.appendChild(hairDiv);
+hairTitle.innerText = "Hair Profesionals Near You"
 
 //Filter Element Div
 const filterElement = document.createElement("div");
 filterElement.classList.add("filter-h2");
+filterElement.innerText = "Filters";
 const filterBox = document.createElement("div");
 filterBox.classList.add("filter-box");
 const filterButtonContainer = document.createElement("div");
@@ -14,7 +18,7 @@ filterBox.appendChild(filterButtonContainer);
 
 //Filter Buttons
 const showAllButton = document.createElement("button");
-showAllButton.classList.add("btn active");
+showAllButton.classList.add("btn-active");
 showAllButton.innerText = "Show all";
 
 const onlineSchButton = document.createElement("button");
@@ -56,6 +60,12 @@ extraFilterBtnOne.innerText = "Extra Filters";
 const extraFilterBtnTwo = document.createElement("button");
 extraFilterBtnTwo.classList.add("btn");
 extraFilterBtnTwo.innerText = "Extra";
+
+const breakPoint = document.createElement("br");
+const breakPoint1 = document.createElement("br");
+const breakPoint2 = document.createElement("br");
+const breakPoint3 = document.createElement("br");
+
 filterButtonContainer.appendChild(showAllButton);
 filterButtonContainer.appendChild(onlineSchButton);
 filterButtonContainer.appendChild(openOnWeekButton);
@@ -68,9 +78,11 @@ filterButtonContainer.appendChild(highestPriceBtn);
 filterButtonContainer.appendChild(extraFilterBtnOne);
 filterButtonContainer.appendChild(extraFilterBtnTwo);
 filterBox.appendChild(filterButtonContainer);
-hairDiv.appendChild(filterBox);
-hairDiv.appendChild(filterElement);
 hairDiv.appendChild(hairTitle);
+hairDiv.appendChild(filterElement);
+hairDiv.appendChild(breakPoint);
+hairDiv.appendChild(filterBox);
+hairDiv.appendChild(breakPoint1);
 
 //Content Cards 
 const contentCardElement = document.createElement("div");
@@ -89,73 +101,50 @@ const contentCardRatingAndLoc = document.createElement("div");
 contentCardRatingAndLoc.classList.add("content-cards_ratingAndLocation"); 
 
 const cardSpan1 = document.createElement("span");
+cardSpan1.innerText = "Angelo Moore ";
 // This is where the artists name will be
 const cardSpan2 = document.createElement("span");
+cardSpan2.innerText = "Rating: 5 stars";
 // This is where the rating will be listed
 const cardSpan3 = document.createElement("span");
+cardSpan3.innerText = "Location: Columbus";
 // This is where the location will be listed
 
 const cardTitle = document.createElement("h1");
 cardTitle.classList.add("content-cards_title");
-cardTitle.setAttribute("text");
+cardTitle.innerText = "Barber";
 
 const cardText = document.createElement("p");
 cardText.classList.add("content-cards_text");
-cardText.setAttribute("text");
+cardText.innerText = "The studio has been a part of the neighborhood for more than five years, and its name comes from a brand of double-edged razors that had been popular in the 1950s and 1960s. The team commits to delivering affordable, high-quality haircuts, and it focuses on blending, fading, and tapering";
 
-const cardCta = document.createElement("p");
+const cardCta = document.createElement("a");
 cardCta.classList.add("content-cards_cta");
-contentCardInfo.appendChild(cardCta);
-contentCardInfo.appendChild(cardText);
-contentCardInfo.appendChild(cardTitle);
-contentCardRatingAndLoc.appendChild(cardSpan3);
-contentCardRatingAndLoc.appendChild(cardSpan2);
+cardCta.innerText = "Read more";
+
+
 contentCardRatingAndLoc.appendChild(cardSpan1);
+contentCardRatingAndLoc.appendChild(breakPoint2);
+contentCardRatingAndLoc.appendChild(cardSpan2);
+contentCardRatingAndLoc.appendChild(breakPoint3);
+contentCardRatingAndLoc.appendChild(cardSpan3);
+
 contentCardInfo.appendChild(contentCardRatingAndLoc);
+contentCardInfo.appendChild(cardTitle);
+contentCardInfo.appendChild(cardText);
+contentCardInfo.appendChild(cardCta);
 contentCardImg.appendChild(cardImg);
-contentCardElement.appendChild(contentCardInfo);
 contentCardElement.appendChild(contentCardImg);
+contentCardElement.appendChild(contentCardInfo);
 hairDiv.appendChild(contentCardElement);
 
 
-// FOOTER ELEMENT
-const footer = document.createElement("div");
-footer.classList.add("footer");
+return hairDiv;
+}
 
-const footerBtn = document.createElement("div");
-footerBtn.classList.add("button");
-
-const footerContainer = document.createElement("div");
-footerContainer.classList.add("container");
-
-const footerCont = document.createElement("div");
-footerCont.classList.add("cont");
-
-const footerCenter = document.createElement("div");
-footerCenter.classList.add("footer_center");
-
-const footerTitle = document.createElement("h3");
-footerTitle.classList.add("footer-title");
-
-const footerContent = document.createElement("ul");
-footerContent.classList.add("footer-content");
-
-const footerLinks = document.createElement("a");
-footerLinks.classList.add("footer-links");
-
-const copyright = document.createElement("li");
-copyright.classList.add("copyright");
-
-footerCenter.appendChild(copyright);
-footerContent.appendChild(footerLinks);
-footerCenter.appendChild(footerContent);
-footerCenter.appendChild(footerTitle);
-footerCont.appendChild(footerCenter);
-footerContainer.appendChild(footerCont);
-footer.appendChild(footerContainer);
-footer.appendChild(footerBtn);
-hairDiv.appendChild(footer);
-
+export{
+    contentPage
+};
 
 
 
