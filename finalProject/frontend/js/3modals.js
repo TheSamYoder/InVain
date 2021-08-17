287cf92b58f71f7f30a85ed05a7ad849a2fb80c4
1mohamed2 <84339105+1mohamed2@users.noreply.github.com>
2021-08-17 11:35:57 -0400

    modals working maybe

--- finalProject/frontend/js/3modals.js
+++ finalProject/frontend/js/3modals.js
@@ -1,11 +1,35 @@
-// const open = document.getElementById(' open')
-// const modal = document.getElementById('modal')
-// const close = document.getElementById('close')
 
-// open.addEventListener('click', () => {
-//     modal.classList.add("show");
-// });
 
-// close.addEventListener('click', () => {
-//     modal.classList.add("show");
-// });
+const open = document.getElementById('open')
+const open2 = document.getElementById('open2');
+const open3 = document.getElementById('open3');
+const modal = document.getElementById('modal');
+const modal2 = document.getElementById('modal2');
+const modal3 = document.getElementById('modal3');
+const close = document.getElementById('close');
+const close2 = document.getElementById('close2');
+const close3 = document.getElementById('close3');
+
+open.onclick = function() {
+    modal.style.display = "block";
+}
+
+open2.onclick = function () {
+    modal2.style.display = "block";
+}
+
+open3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+close.onclick = function () {
+    modal.style.display = "none";
+}
+
+close2.onclick = function () {
+    modal2.style.display = "none";
+}
+
+close3.onclick = function () {
+    modal3.style.display = "none";
+}
