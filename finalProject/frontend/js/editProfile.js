c38c6816d7edeb7b6410bedf72a4d9a5a26ba7b8
HEWiggins <HE.Wiggins1990@gmail.com>
2021-08-18 14:16:07 -0400

    centered name button on edit & fixed modals on page

--- finalProject/frontend/js/editProfile.js
+++ finalProject/frontend/js/editProfile.js
@@ -30,44 +30,141 @@ const editArtistInfo = buildElement("p", "edit-artist-info", professional.descri
 const editBtnArtistInfo = buildElement("button", "editBtn-experience", "Edit About Artist");
   // ------------------Modal Container----------------------------
 
-  const editModalContainer = document.createElement("div");
-  editModalContainer.classList.add("edit-modal-container");
+  const modalContainer = document.createElement("div");
+  modalContainer.classList.add("modal-container");
   const scheduleModal = document.createElement("div");
-  scheduleModal.classList.add("modals");
+  scheduleModal.classList.add("openModal");
+  scheduleModal.id = "modal";
 
   const scheduleBtn = document.createElement("button");
-  scheduleBtn.innerText = "Scheduling / Hours of Opp";
+  scheduleBtn.innerText = "Scheduling / Business Hours";
   scheduleBtn.classList.add("content-cards_cta");
-  scheduleBtn.addEventListener("click", () => {
-      const scheduleTab = window.open('http://google.com', '_self');
-  })
+  scheduleBtn.id = "open";
+  scheduleBtn.onclick = function () {
+    modal.style.display = "block";
+  };
 
-  const reviewModal = document.createElement("div");
+  const scheduleModalContent = buildElement("div", "modal-content");
+  const scheduleTitle = document.createElement("h1");
+  scheduleTitle.innerText = "Schedule";
+  const scheduleModalText = document.createElement("p");
+  const mondayWord = document.createElement("small");
+  mondayWord.innerText = "Monday - 9am-6pm";
+  const tuesdayWord = document.createElement("small");
+  tuesdayWord.innerText = "Tuesday - 9am-6pm";
+  const wednesdayWord = document.createElement("small");
+  wednesdayWord.innerText = "Wednesday - 9am-6pm";
+  const thursdayWord = document.createElement("small");
+  thursdayWord.innerText = "Thursday - 9am-6pm";
+  const fridayWord = document.createElement("small");
+  fridayWord.innerText = "Closed";
+  const saturdayWord = document.createElement("small");
+  saturdayWord.innerText = "Saturday - 9am-6pm";
+  const sundayWord = document.createElement("small");
+  sundayWord.innerText = "Sunday - 9am-6pm";
+
+  const scheduleBtnClose = buildElement("button", undefined, "Close Me");
+  scheduleBtnClose.id = "close";
+  scheduleBtnClose.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  scheduleModalText.append(
+    mondayWord,
+    tuesdayWord,
+    wednesdayWord,
+    thursdayWord,
+    fridayWord,
+    saturdayWord,
+    sundayWord
+  );
+  scheduleModalContent.append(
+    scheduleTitle,
+    scheduleModalText,
+    scheduleBtnClose
+  );
+  scheduleModal.append(scheduleModalContent);
+  modalContainer.append(scheduleBtn, scheduleModal);
+
+  const socialModal = document.createElement("div");
+  socialModal.classList.add("openModal");
+  socialModal.id = "modal2";
+  const socialsBtn = buildElement(
+    "button",
+    "socialsModal",
+    "Social Media/Contact Me"
+  );
+  socialsBtn.id = "open2";
+  socialsBtn.classList.add("content-cards_cta");
+  socialsBtn.onclick = function () {
+    modal2.style.display = "block";
+  };
+  const socialsModalContent = buildElement("div", "modal-content");
+  const socialsTitle = document.createElement("h1");
+  socialsTitle.innerText = "Contact Me"
+  const twitterLink = document.createElement("a");
+  twitterLink.href = "https://twitter.com";
+  twitterLink.alt = "Twitter Link";
+  const twitterImage = document.createElement("img");
+  twitterImage.classList.add("social-image");
+  twitterImage.src = "https://bit.ly/3CVGjRG";
+  twitterImage.alt = "Twitter Logo";
+  twitterImage.setAttribute("width", "50");
+  const instaLink = document.createElement("a");
+  instaLink.classList.add("social-image1");
+  instaLink.href = "https://instagram.com";
+  instaLink.alt = "Instagram Link";
+  const instagramImage = document.createElement("img");
+  instagramImage.classList.add("social-image1");
+  instagramImage.src = "https://bit.ly/2UvY6xy";
+  instagramImage.alt = "Instagram logo";
+  instagramImage.setAttribute("width", "50");
+  const contactInfo = buildElement("div", "contact-me", "Call Me: " + professional.phoneNumber);
+  const socialsBtnClose = buildElement("button", undefined, "Close Me");
+  socialsBtnClose.id = "close2";
+  socialsBtnClose.onclick = function () {
+    modal2.style.display = "none";
+  };
+
+  socialsModalContent.append(
+    socialsTitle,
+    twitterLink,
+    twitterImage,
+    instaLink,
+    instagramImage,
+    contactInfo,
+    socialsBtnClose
+  );
+  socialModal.append(socialsModalContent);
+  modalContainer.append(socialsBtn, socialModal);
+
+  const reviewModal = buildElement("div", "openModal");
   reviewModal.classList.add("modals");
-  const reviewBtn = document.createElement("button");
-  reviewBtn.innerText = "Review & Rating";
-  reviewBtn.classList.add("content-cards_cta");
-  reviewBtn.addEventListener("click", () => {
-      const reviewTab = window.open('http://google.com', '_self');
-  })
+  reviewModal.id = "modal3";
+  const reviewsBtn = document.createElement("button");
+  reviewsBtn.innerText = "Reviews";
+  reviewsBtn.id = "open3";
+  reviewsBtn.classList.add("content-cards_cta");
+  reviewsBtn.onclick = function () {
+    modal3.style.display = "block";
+  };
+  const reviewsModalContent = buildElement("div", "modal-content");
+  const reviewsTitle = buildElement("h1", undefined, "Reviews");
+  const reviewsTextBox = buildElement("div", "reviews-text-box");
+  professional.reviews.forEach((review) => {
+    const reviewText = buildElement("div", "review", professional.review);
+    reviewsTextBox.append(reviewText);
+  });
+  const reviewBtnClose = buildElement("button", undefined, "Close Me");
+  reviewBtnClose.id = "close3";
+  reviewBtnClose.onclick = function () {
+      modal3.style.display = "none";
+      
+  };
 
-  const messageModal = document.createElement("div");
-  messageModal.classList.add("modals");
-  const messageBtn = document.createElement("button");
-  messageBtn.innerText = "Message Me";
-  messageBtn.classList.add("content-cards_cta");
-  messageBtn.addEventListener("click", () => {
-      const messageTab = window.open('http://google.com', '_self');
-  })
-
-  const lastModal = document.createElement("div");
-  lastModal.classList.add("modals");
-  const lastBtn = document.createElement("button");
-  lastBtn.innerText = "Last Modal";
-  lastBtn.classList.add("content-cards_cta");
-  lastBtn.addEventListener("click", () => {
-      const lastTab = window.open('http://google.com', '_self');
-  })
+  reviewsModalContent.append(reviewsTitle, reviewsTextBox, reviewBtnClose);
+  reviewModal.append(reviewsModalContent);
+  modalContainer.append(reviewsBtn, reviewModal);
 
 
   // ------------------Map----------------------------
@@ -103,12 +200,7 @@ const editBtnArtistInfo = buildElement("button", "editBtn-experience", "Edit Abo
 serviceContainer.appendChild(services);
 editContentCardImg.appendChild(editArtistImg, editBtnArtistPhoto);
 editContentCardsTitle.append(editOccupation, editBtnOccupation, editExperience, editBtnExperience, overallRating, editLocation, editBtnLocation, aboutArtist, editArtistInfo, editBtnArtistInfo);
-scheduleModal.appendChild(scheduleBtn);
-reviewModal.appendChild(reviewBtn);
-messageModal.appendChild(messageBtn);
-lastModal.appendChild(lastBtn);
-editModalContainer.append(scheduleModal, reviewModal, messageModal, lastModal);
-editContentCardInfo.append(editContentCardsTitle, editModalContainer);
+editContentCardInfo.append(editContentCardsTitle, modalContainer);
 map.appendChild(iframe);
 mapContainer.append(locationTitle, map);
 contentCards.append(editContentCardImg, editContentCardInfo, mapContainer);
